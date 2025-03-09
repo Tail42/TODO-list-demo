80535ad7d4cc2577719490ebde32f3b5044ef3c9
unknown <changtail42@gmail.com>
2025-03-09 16:37:23 +0800

    upload remain files

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import TodoItem from './components/TodoItem.vue';
+</script>
+
+<template>
+  <h1>TODO LIST</h1>
+  <TodoItem></TodoItem>
+</template>
+
+
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,4 @@
+<script setup>
+    console.log("hello here")
+</script>
+
